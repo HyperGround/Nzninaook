375d9016fc89709c9176d440e75badd397048efd
HyperGround <112865572+HyperGround@users.noreply.github.com>
2024-01-28 19:22:23 +0300

    Update config.js

--- config.js
+++ config.js
@@ -24,11 +24,13 @@ pmodule.exports = {
             roleID: "1194623453484028004"
         }
     ],
-    welcomeMessage: "╭ ・ ⌬ ・{member} joined.
+    welcomeMessage: "
+╭ ・ ⌬ ・{member} joined.
 ●▬▬▬▬▬▬▬▬๑۩✰۩๑▬▬▬▬▬▬▬▬●
 ✰・Invited by **{inviter}**
 ✰・They have now **{invites}** invites
 ●▬▬▬▬▬▬▬▬๑۩✰۩๑▬▬▬▬▬▬▬▬●
-╰ ・ ⌬ ・**BOT STATION COMMUNITY** ", // Use {inviter} for who invited the member, {member} for the member, {code} for the invite used, {mention} to mention the inviter, {ID} for the ID of the member, {inviterID} for the inviter's ID, and {invites} for the inviter's invites
+╰ ・ ⌬ ・**BOT STATION COMMUNITY** 
+", // Use {inviter} for who invited the member, {member} for the member, {code} for the invite used, {mention} to mention the inviter, {ID} for the ID of the member, {inviterID} for the inviter's ID, and {invites} for the inviter's invites
     leaveMessage: "{member} left the server. They were invited by **${inviter}** (who has ${invites} invites).", // Use {inviter} for who invited the member, {member} for the member, {mention} to mention the inviter, {ID} for the ID of the member, {inviterID} for the inviter's ID, and {invites} for the inviter's invites
 }
